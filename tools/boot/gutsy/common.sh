790ca6b55dc791648d44f812362980da52e8ed45
Colin Watson <cjwatson@canonical.com>
2007-07-04 14:56:31 +0100

    gnubuntu -> gobuntu, per Mark

--- tools/boot/gutsy/common.sh
+++ tools/boot/gutsy/common.sh
@@ -30,8 +30,8 @@ default_preseed() {
 	xubuntu)
 	    DEFAULT_PRESEED='file=/cdrom/preseed/xubuntu.seed'
 	    ;;
-	gnubuntu)
-	    DEFAULT_PRESEED='file=/cdrom/preseed/gnubuntu.seed'
+	gobuntu)
+	    DEFAULT_PRESEED='file=/cdrom/preseed/gobuntu.seed'
 	    ;;
 	ubuntu-server)
 	    DEFAULT_PRESEED='file=/cdrom/preseed/ubuntu-server.seed'
