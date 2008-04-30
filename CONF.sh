2aa0cae07a3dbcc4a5bc45c83b5beb4474fa99b8
Colin Watson <cjwatson@canonical.com>
2008-04-30 22:54:02 +0100

    edgy|feisty|gutsy += security,updates; hardy += security,updates,proposed

--- CONF.sh
+++ CONF.sh
@@ -228,20 +228,20 @@ esac
 # Where to find the security patches.  This directory should be the
 # top directory of a security.debian.org mirror.
 case $DIST in
-  warty|hoary|breezy|dapper)
+  warty|hoary|breezy|dapper|edgy|feisty|gutsy)
     export SECURITY="$MIRROR"
     ;;
 esac
 
 # Use post-release updates?
 case $DIST in
-  warty|hoary|breezy|dapper)
+  warty|hoary|breezy|dapper|edgy|feisty|gutsy)
     export UPDATES=1
     ;;
 esac
 
 case $DIST in
-  dapper)
+  hardy)
     export PROPOSED=1
     ;;
 esac
