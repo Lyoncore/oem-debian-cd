effc6ef0cbf7ab225b0ef14632ca22aea6db3711
Colin Watson <cjwatson@canonical.com>
2010-09-03 09:11:18 +0100

    maverick back to Alpha post-beta-release

--- CONF.sh
+++ CONF.sh
@@ -157,7 +157,7 @@ case $DIST in
     export CODENAME=maverick
     export CAPCODENAME='Maverick Meerkat'
     export DEBVERSION=10.10
-    export OFFICIAL="Beta"
+    export OFFICIAL="Alpha"
     ;;
 esac
 
