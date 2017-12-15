3309dc40faace3b4010b5e3e1bb0d74304fbd26d
Woodrow Shen <woodrow.shen@canonical.com>
2017-12-15 15:01:56 +0800

    Add specific folder/file for argos-desktop

--- build_all.sh
+++ build_all.sh
@@ -108,6 +108,8 @@ do
 			make bin-extras CD=1 ROOTSRC=$CDIMAGE_ROOT/oem-cdimage-script/ DIR=postinstall
 			make bin-extras CD=1 ROOTSRC=$CDIMAGE_ROOT/oem-cdimage-script/ DIR=pool
 			make bin-extras CD=1 ROOTSRC=$CDIMAGE_ROOT/oem-cdimage-script/ DIR=boot
+			make bin-extras CD=1 ROOTSRC=$CDIMAGE_ROOT/oem-cdimage-script/ DIR=target_files
+			make bin-extras CD=1 ROOTSRC=$CDIMAGE_ROOT/oem-cdimage-script/ DIR=set-version.sh
 			. $CDIMAGE_ROOT/oem-cdimage-script/clean-pool.sh
 		fi
 		make bin-official_images $SIZE_ARGS SRCSIZELIMIT=$FULL_SIZE
