6d8558b99b1524b6adda811a5db71aebb15f65fc
Adam Conrad <adconrad@0c3.net>
2019-02-18 08:20:17 +0000

    bionic: Release->Beta

--- CONF.sh
+++ CONF.sh
@@ -277,7 +277,7 @@ case $DIST in
     export CAPCODENAME='Bionic Beaver'
     export DEBVERSION="18.04.2 LTS"
     export BACKPORT_KERNEL="hwe-18.04"
-    export OFFICIAL="Release"
+    export OFFICIAL="Beta"
     ;;
   cosmic)
     export PREV_CODENAME=bionic
